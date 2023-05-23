29806d5bfff09257a390a686e511271806fd278d
natanap <natanalvespr@gmail.com>
2023-05-23 20:32:22 -0300

    up project

--- app/Controllers/Admin/Home.php
+++ app/Controllers/Admin/Home.php
@@ -8,6 +8,11 @@ class Home extends BaseController
 {
     public function index()
     {
-        //
+        $data = [
+            'titulo' => 'Home da área restrita'
+        ];
+
+        return view('Admin/Home/index', $data);
     }
 }
+aula 18
